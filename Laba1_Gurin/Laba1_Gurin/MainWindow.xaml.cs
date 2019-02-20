0f3cbf23c720a881e140967cf5816182caead90c
unknown <ival3446@gmail.com>
2019-02-20 13:19:06 +0200

    sec

---
+++ Laba1_Gurin/Laba1_Gurin/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba1_Gurin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = DatePicker1.SelectedDate.GetType().ToString();
+
+            TextBlock1.Text = text;
+        }
+        private void DatePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+            string text = DatePicker1.SelectedDate.ToString();
+            TextBlock1.Text = text;
+        }
+    }
+}
